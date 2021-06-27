20bdad2fdcd02daae20b9aaf1a81923a44addbb5
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2021-06-28 00:30:47 +1200

    setup mongodb connection

--- pages/api/blog-api.js
+++ pages/api/blog-api.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello world' })
+import { connectToDatabase } from '../../util/connection'
+
+export default async function getPosts(req, res) {
+    const { db } = await connectToDatabase();
+
+    const posts = await db
+        .collection("posts")
+        .find({})
+        .limit(20)
+        .toArray();
+
+    res.json(posts)
 }
