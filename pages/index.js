0641ef97289f4383731d14f4524eabe0c45f0425
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2021-11-26 02:05:35 +1300

    pagenation on bottom

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { useBlogContext } from '../context/PostProvider'
 import Article from '../components/Article';
-import Link from 'next/link';
-import utilStyles from '../styles/utilStyles.module.css'
+import Pagination from '../components/Pagination';
 
 export default function Home() {
   const { posts, page, maxPage, pageNumbers } = useBlogContext();
@@ -17,43 +16,7 @@ export default function Home() {
           </Article>
         )
       })}
-      
-      <Link href={{
-        pathname: '/',
-        query: { page: 1 }
-      }}>      
-        <span className={utilStyles.link}>{page != 1 ? "<<" : ''}</span>
-      </Link>
-
-      {pageNumbers.map(_ => {
-        console.log(page)
-        console.log(_)
-        return (
-          <Link 
-            key={_}
-            href={{
-            pathname: '/',
-            query: { page: _ }
-          }}>
-            {page == _
-              ? <span className={utilStyles.currentLink}>
-                {_}
-              </span>
-              : <span className={utilStyles.link}>
-                {_}
-              </span>
-            }
-          </Link>
-        )
-      })}
-      
-      <Link href={{
-        pathname: '/',
-        query: { page: maxPage }
-      }}>      
-        <span className={utilStyles.link}>{page != maxPage ? ">>" : ''}</span>
-      </Link>
-
+      <Pagination page={page} pageNumbers={pageNumbers} maxPage={maxPage}/>
     </Layout>
   )
 }
